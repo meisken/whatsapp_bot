18fbc7a11e9d264caaa057445b6bbfd211a5e8bf
meisken <meisken999@gmail.com>
2024-02-10 18:03:17 +0800

    add: save message to json function

--- app.ts
+++ app.ts
@@ -3,10 +3,13 @@ import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import qrcode from "qrcode-terminal"
 import { Client, LocalAuth, MessageMedia } from "whatsapp-web.js"
-import fs from "fs"
+import { promises as fs } from 'fs';
 import https from "https"
 import cors from "cors";
 import { removeSpace } from './utility/removeSpace';
+import { updateJsonData } from './utility/updateJson';
+import { getCurrentDate } from './utility/getCurrentDate';
+import { getFileExtensionFromMime } from './utility/getFileExtensionFromMime';
 
 // const key = fs.readFileSync('./cert/key.pem');
 
@@ -37,7 +40,7 @@ client.on("authenticated", () => {
     console.log("AUTHENTICATED");
 }); 
 
-const commandDispatcher = process.env.commandDispatcher
+const commandDispatcher = process.env.commandDispatcher!
 export interface actionParams{
 
 }
@@ -57,7 +60,7 @@ client.on("message", async (message) => {
  
 
     const msg = message.body;
-    
+    const jsonFilepath = "./json/message.json";
     type Prefixes = "command:";
     const prefixes: {[key: string]: Prefixes} = {
         command: "command:",
@@ -92,10 +95,53 @@ client.on("message", async (message) => {
         
         }
     }
-    whatsappCommander();
+    //whatsappCommander();
 
+    const saveMessageToJson = async () => {
+        console.log(message.body)
+        const currentTime = getCurrentDate(true);
+        const mediaFolderPath = "./assets/"
+        if(message.author !== undefined) {
+            return
+            //ignore group message
+        }
+        if(message.hasMedia){
+            const media = await message.downloadMedia();
+    
+            let filename = media.filename;
+            try{
+                if(media.filename === undefined || media.filename === null){
+                    filename = `${currentTime}.${getFileExtensionFromMime(media.mimetype)}`;
+                 
+                    await fs.writeFile(`${mediaFolderPath}/${filename}`, media.data, { encoding: "base64", flag: "wx"})
+                    updateJsonData({
+                        contact: message.from,
+                        hasMedia: true,
+                        jsonFilepath,
+                        message: message.body,
+                        mediaFilename: filename
+                    })
+               
+                }else{
+                    throw new Error("media filename is undefined")
+                }
+            }catch(err){
+                client.sendMessage(commandDispatcher, `save media error: ${err}`)
+            }
+  
+     
+         
+        }else{
+            updateJsonData({
+                contact: message.from,
+                hasMedia: false,
+                jsonFilepath,
+                message: message.body
+            })
+        }
+    }
 
-
+    saveMessageToJson()
 
 });
 
